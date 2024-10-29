ba0c7f571c8330174b9bf93884df877b7464f2b9
NambuyaGloria <glowieglosh@gmail.com>
2024-10-29 10:13:54 +0300

    MYSQL WORK
    This describes steps on how to create tables and use different features while using visual studio code

---
+++ uffe.sql
@@ -0,0 +1,10 @@
+-- Active: 1728117029218@@127.0.0.1@3306@uffe
+CREATE DATABASE;
+SHOW DATABASES;
+USE uffe;
+show tables;
+select * from farmers;
+show tables;
+select * from crop;
+SELECT * FROM farmer_crop;
+
